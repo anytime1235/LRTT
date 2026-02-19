e87101cdd58ee158abf93f01c30ba4f7be177128
User <user@example.com>
2026-02-19 20:13:16 +0000

    Widen ALBERT SQuAD search ranges and add new Optuna DB

--- examples/albert/optuna_albert_squad_lrtt.py
+++ examples/albert/optuna_albert_squad_lrtt.py
@@ -926,7 +926,7 @@ def objective(trial, train_loader, eval_features, eval_examples, tokenizer):
         torch.cuda.empty_cache()
 
     # Hyperparameters
-    learning_rate = trial.suggest_float('learning_rate', 3e-2, 2e1, log=True)
+    learning_rate = trial.suggest_float('learning_rate', 3e-3, 2e2, log=True)
 
     # LRTT parameters: skip sweep if --no-transfer (A/B frozen, no transfer happens)
     if OPT_CONFIG['no_transfer']:
@@ -937,11 +937,11 @@ def objective(trial, train_loader, eval_features, eval_examples, tokenizer):
         lora_alpha = 1.0         # fixed (no effect)
         tau_sec = 0.0            # fixed
     else:
-        transfer_lr = trial.suggest_float('transfer_lr', 4e-6, 8e-3, log=True)
+        transfer_lr = trial.suggest_float('transfer_lr', 4e-7, 8e-2, log=True)
         transfer_every = trial.suggest_int('transfer_every', 63, 35000, log=True)
         rank_exp = trial.suggest_int('rank_exp', 0, 5)
         rank = 2 ** rank_exp
-        lora_alpha = trial.suggest_float('lora_alpha', 6e-5, 2e1, log=True)
+        lora_alpha = trial.suggest_float('lora_alpha', 6e-6, 2e2, log=True)
         tau_sec = trial.suggest_float('tau_sec', 0, 0, log=False)  # 0 = no decay
 
     # C tile pulsed transfer params (only meaningful for onehot/direct)
